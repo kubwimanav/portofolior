a972cec3851ed4e02c8f83a5d67673383899d4b6
kubwimanav <kubwimanavincent2020@gmail.com>
2024-03-27 22:02:49 +0200

    my portofolio

--- src/Dashboard/DashNavbar.jsx
+++ src/Dashboard/DashNavbar.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react'
 import { MdMenu } from "react-icons/md";
+import { CiSearch } from "react-icons/ci";
 
 import { IoArrowBackCircleOutline } from "react-icons/io5";
 import './Dashstyles/Dashnavbar.css'
 function DashNavbar() {
   return (
     <div className="navbar">
-      <div style={{display:'flex', gap:'20px'}}>
+      <div style={{ display: "flex", gap: "20px" }}>
         <MdMenu />
         <p>Vin© 2024</p>
+        <div>
+          <input type="text" placeholder="search"  className='search'/>
+          <CiSearch  className='cisearch'/>
+        </div>
       </div>
 
       <p>vincent@gmail.com</p>
